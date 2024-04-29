44984e42c0b825cf9d8013ae5c3e7ad2a34f1248
veeramshetti <165231167+veeramshetti@users.noreply.github.com>
2024-04-29 20:34:36 +0530

    Add files via upload

---
+++ coad-alpha/script.js
@@ -0,0 +1,14 @@
+function calculateAge() {
+    var dobInput = document.getElementById("dob").value;
+    var dob = new Date(dobInput);
+    var now = new Date();
+    
+    var age = now.getFullYear() - dob.getFullYear();
+    var monthDiff = now.getMonth() - dob.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && now.getDate() < dob.getDate())) {
+        age--;
+    }
+    
+    document.getElementById("result").innerHTML = "Your age is: " + age;
+}
